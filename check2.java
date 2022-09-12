3c01a826c83e3ddf31d06b17325eea13f021757e
dhivyara <dhivyaraja2010@gmail.com>
2022-09-12 21:20:44 +0530

    updation

--- check2.java
+++ check2.java
@@ -1,3 +1,9 @@
 Hi this file will be created as second file
+<<<<<<< HEAD
+
+
+
 this is modification from master branch
 now, going to modify from dev branch 
+=======
+>>>>>>> 41aa8be83ce521fceb11af400154e98ed81ab15e
